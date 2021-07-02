e2dc26319c71d4600cf0b75fea1ab7ea63195fba
alexisraelmtz <alex.israel.mtz@gmail.com>
2021-07-01 20:41:09 -0500

    fixed Issue #R6C-13 Create Element - Frontend component added

--- components/Canva.js
+++ components/Canva.js
@@ -1,5 +1,64 @@
-const Canva = () => {
-  return <p>Hi</p>;
-};
+import React, { useRef, useEffect, useState } from "react";
+import '../styles/canvaStyles.module.scss';
 
-export default Canva;
+
+function MyCanva() {
+  const canvasRef = useRef(null)
+  const contextRef = useRef(null)
+  const [isDrawing, setIsDrawing] = useState(false)
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 2;
+    canvas.height = window.innerHeight * 2;
+    canvas.style.height = `${window.innerHeight}px`;
+    canvas.style.width = `${window.innerWidth}px`;
+
+    const context = canvas.getContext("2d");
+    context.scale(2, 2);
+    context.lineCap = "round";
+    context.strokeStyle = "black";
+    context.lineWidth = 5;
+    contextRef.current = context;
+  }, []);
+
+  const startDrawing = (nativeEvent) => {
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.beginPath();
+    contextRef.current.moveTo(offsetX, offsetY);
+    setIsDrawing(true);
+  }
+
+
+  const stopDrawing = () => {
+    contextRef.current.closePath();
+    setIsDrawing(false);
+  }
+
+
+  const draw = ({ nativeEvent }) => {
+    if (!isDrawing) {
+      return
+    };
+    const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.lineTo(offsetX, offsetY);
+    contextRef.current.stroke();
+  }
+
+
+  return (
+    <canvas
+      height="300px"
+      onMouseDown={startDrawing}
+      onMouseUp={stopDrawing}
+      onMouseMove={draw}
+      ref={canvasRef}
+    />
+  );
+}
+
+// 1 boton Clear
+// 1 boton Save
+// I am a dumb fack
+
+export default MyCanva;
--- pages/listCanvas.js
+++ pages/listCanvas.js
@@ -1,7 +1,11 @@
 // Pending /pages/ structure to be defined with pseudoCode.
 // help: Thread #03 ===> Agile.MD
+
 import { signOut } from "next-auth/client";
 import Nav from "../components/Nav";
+import MyCanva from '../components/Canva'
+
+
 const ListCanvas = () => {
   return (
     <div>
@@ -11,6 +15,7 @@ const ListCanvas = () => {
         onClick={() => signOut({ callbackUrl: "http://localhost:3000/" })}>
         Sign out
       </button>
+      <MyCanva></MyCanva>
     </div>
   );
 };
