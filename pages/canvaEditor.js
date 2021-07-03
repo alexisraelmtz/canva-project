4d6b9cd9f99f7ec5b0a1ae66674439dfd57b8ee5
alexisraelmtz <alex.israel.mtz@gmail.com>
2021-07-03 06:47:08 -0500

    Issues Fixed with Styles and Components

--- pages/canvaEditor.js
+++ pages/canvaEditor.js
@@ -1,7 +1,7 @@
 // Pending /pages/ structure to be defined with pseudoCode.
 // help: Thread #03 ===> Agile.MD
 import MyCanva from '../components/Canva'
-import mySession from '../components/mySession'
+// import mySession from '../components/mySession'
 import styles from "../styles/CanvaEditor.module.scss"
 
 
